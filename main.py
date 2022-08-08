38ed7dae5c5f2e7dd74f21c17bbb747937d50745
Mal-Ourisso <toinho99@gmail.com>
2022-08-08 09:35:54 -0300

    Added main file

---
+++ main.py
@@ -0,0 +1,93 @@
+
+import tensorflow as tf
+import tensorflow_addons as tfa
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+from tensorflow.keras import layers, models, optimizers, preprocessing, applications, utils
+from random import randint
+import os
+import json
+import custom_loader as cl
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]= "3"
+
+# SETTING PARAMETERS
+
+seed = 928347
+tam = 396
+image_size = (tam, tam)
+batch_size = 128
+shape = (tam, tam, 3)
+epochs = 1
+validation_split = .05
+class_names = ["NORMAL", "CNV", "DME", "DRUSEN"]
+
+# GENERATE EXPERIMENT
+
+modelos = {
+	"Inc" : applications.InceptionV3(weights="imagenet", include_top=False, input_shape=shape),
+	"Res" : applications.ResNet50(weights="imagenet", include_top=False, input_shape=shape),
+	"Vgg" : applications.VGG16(weights="imagenet", include_top=False, input_shape=shape)
+	}
+
+# With/Without fine tuning
+fine_tune = ["Ft", "Wo"]
+
+# Bilinear Interpolation/Zero Padding
+#|||||||define resize as parameter on custom-loader|||||||
+resize = ["interpolation", "zeropadding"]
+
+# 4 Classes; NormalxAnormal; NormalxDme; NormalxDrusen; NormalxCNV
+experiments = ["Clas", "NxAn", "NxDm", "NxDr", "NxCn"]
+
+for model_name, base in modelos.items():
+	#LOADING DATASET
+
+	#||||||||adjust acording to expirement||||||||||||
+
+	train, val = cl.customDataset(
+		"/home/mauricio/dados/Mauricio/OCT2017/train", 
+		validation_split=validation_split, 
+		label_names=class_names, 
+		batch_size=batch_size,
+		h=tam, 
+		w=tam,
+		resize=resize[1]
+		)
+
+	# CREATING MODEL
+
+	base.trainable = False
+
+	jarbas = models.Sequential()
+	jarbas.add(base)
+	jarbas.add(layers.Flatten())
+	#||||||||adjust acording to experiment||||||||||||
+	jarbas.add(layers.Dense(4, activation='softmax'))
+
+	#||||||||adjust acording to experiment||||||||||||
+	jarbas.compile(
+		loss="sparse_categorical_crossentropy", 
+		optimizer="Adam", 
+		metrics=tfa.metrics.CohenKappa(4, sparse_labels=True))
+
+	# TRAINING MODEL
+
+	jarbas.fit(train, epochs=epochs, validation_data=val)
+
+	# TESTING	
+	
+	#||||||||adjust acording to experiment||||||||||||
+	test, _ = cl.customDataset(
+		"/home/mauricio/dados/Mauricio/OCT2017/test", 
+		label_names=class_names, 
+		batch_size=batch_size,
+		h=tam, 
+		w=tam
+		)
+
+	#|||||||do fine tuning|||||||
+
