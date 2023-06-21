0ba14f5d28984aa72652127928d8e41234dc4cc7
Maurício <91477409+Mal-Ourisso@users.noreply.github.com>
2023-06-21 14:26:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,152 +1,78 @@
 
 import tensorflow as tf
-import tensorflow_addons as tfa
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sb
-import joblib
-from tensorflow.keras import metrics, layers, models, optimizers, preprocessing, applications, utils
-from random import randint
-import os
-import json
-import csv
+from tensorflow.keras import applications, models, layers, optimizers, metrics
 import custom_loader as cl
 
+import os
+from math import ceil
+from random import getrandbits
+
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]= "3"
+os.environ["CUDA_VISIBLE_DEVICES"]= "0"
 
-# SETTING PARAMETERS
+# SETTING CONFIG VALUES
 
-seed = 928347
-tam = 396
-image_size = (tam, tam)
-batch_size = 32
-shape = (tam, tam, 3)
+dataset_dir = "OCT-DATABASE/Mult/"
+batch_size = 16
+size = 396
 epochs = 50
-validation_split = .05
-class_names = ["NORMAL", "CNV", "DME", "DRUSEN"]
+fine_epochs = 10
+validation_split = 0.05
+random_seed = getrandbits(64)
 
-# GENERATE EXPERIMENT
+# LOADING DATASET
 
-modelos = {
-	"Inc" : applications.InceptionV3(weights="imagenet", include_top=False, input_shape=shape),
-	"Res" : applications.ResNet50(weights="imagenet", include_top=False, input_shape=shape),
-	"Vgg" : applications.VGG16(weights="imagenet", include_top=False, input_shape=shape)
-	}
+train, val = cl.customDataset(dataset_dir+"train/", rand=random_seed ,validation_split=validation_split, h=size, w=size, batch_size=batch_size)
+test, _ = cl.customDataset(dataset_dir+"test/", rand=False, h=size, w=size, batch_size=batch_size)
 
-def adjust_img(image, label):
-	return image/255, label
+def func(image, label):
+        return image/255, label
 
-for model_name, base in modelos.items():	
+train = train.map(func)
+val = val.map(func)
+test = test.map(func)
 
-	#LOADING DATASET
+# CREATING MODEL FOR TRAINING
 
-	train, val = cl.customDataset(
-		"/home/mauricio/dados/Mauricio/OCT2017/train", 
-		validation_split=validation_split, 
-		label_names=class_names, 
-		batch_size=batch_size,
-		h=tam, 
-		w=tam,
-		resize="interpolation"
-		)
+jarbas = models.Sequential()
+jarbas.add(applications.VGG16(weights="imagenet", include_top=False, input_shape=(size, size, 3)))
+jarbas.add(layers.Flatten())
+jarbas.add(layers.Dense(4, activation="softmax"))
+jarbas.layers[0].trainable = False
 
-	test, _ = cl.customDataset(
-		"/home/mauricio/dados/Mauricio/OCT2017/test", 
-		label_names=class_names, 
-		batch_size=batch_size,
-		h=tam, 
-		w=tam,
-		resize="interpolation"
-		)
+jarbas.compile(
+        loss="sparse_categorical_crossentropy",
+        optimizer="Adam",
+        metrics=["accuracy"])
 
-	train = train.map(adjust_img)
-	val = val.map(adjust_img)
-	test = test.map(adjust_img)
+# NORMAL TRAINING
 
-	# CREATING MODEL
+jarbas.fit(train, validation_data=val, epochs=epochs)
 
-	base.trainable = False
+# SETTING UP FOR FINE TUNING
 
-	jarbas = models.Sequential()
-	jarbas.add(base)
-	jarbas.add(layers.Flatten())
-	jarbas.add(layers.Dense(4, activation='softmax'))
+jarbas.layers[0].trainable = True
 
-	jarbas.compile(
-		loss="sparse_categorical_crossentropy", 
-		optimizer="Adam", 
-		metrics=tfa.metrics.CohenKappa(4, sparse_labels=True))
+jarbas.compile(
+        loss="sparse_categorical_crossentropy",
+        optimizer=optimizers.Adam(learning_rate=10e-5),
+        metrics=["accuracy"])
 
-	# TRAINING MODEL
+# FINE TUNING
 
-	learning_hist = jarbas.fit(train, epochs=epochs, validation_data=val)
+jarbas.fit(train, validation_data=val, epochs=fine_epochs)
 
-	# TESTING	
-	
-	con_mat = []
-	con_mat_tensor = tf.math.confusion_matrix(np.concatenate([label for _,label in test]), np.argmax(jarbas.predict(test), axis=-1))
-	for i in con_mat_tensor:
-		con_mat.append(list(i.numpy()))
+# TESTING
 
-	print(f"{model_name}NO FINE TUNE")
-	jarbas.evaluate(teste)
+con_mat = []
+con_mat_tensor = tf.math.confusion_matrix(np.concatenate([label for _,label in test]), np.argmax(jarbas.predict(test), axis=-1))
+for i in con_mat_tensor:
+        con_mat.append(list(i.numpy()))
 
-	#SAVING
-	
-	dir_name = model_name+"0/"
+print(*con_mat)
+jarbas.evaluate(test)
 
-	try:
-		os.mkdir(dir_name)
-	except:
-		pass
+# SAVING
 
-	with open(dir_name+'learning_hist.json', 'w') as f:
-		json.dump(learning_hist.history, f)
-	
-	jarbas.save(dir_name+"saved_model")
-
-	with open(dir_name+"confusion_matrix.csv", 'w') as csvfile:
-		csvwriter = csv.writer(csvfile)
-		csvwriter.writerows(con_mat)
-	
-	# FINE TUNING
-
-	jarbas.layers[0].trainable = True
-
-	jarbas.compile(
-		loss="sparse_categorical_crossentropy", 
-		optimizer="Adam", 
-		metrics=tfa.metrics.CohenKappa(4, sparse_labels=True))
-
-	learning_hist = jarbas.fit(train, epochs=epochs//10, validation_data=val)
-
-	# TESTING FINE TUNE
-		
-	con_mat = []
-	con_mat_tensor = tf.math.confusion_matrix(np.concatenate([label for _,label in test]), np.argmax(jarbas.predict(test), axis=-1))
-	for i in con_mat_tensor:
-		con_mat.append(list(i.numpy()))
-
-	print(f"{model_name}FINE TUNE")
-	jarbas.evaluate(teste)
-
-	#SAVING FINE TUNE
-	
-	dir_name = model_name+"1/"
-
-	try:
-		os.mkdir(dir_name)
-	except:
-		pass
-
-	with open(dir_name+'learning_hist.json', 'w') as f:
-		json.dump(learning_hist.history, f)
-	
-	jarbas.save(dir_name+"saved_model")
-
-	with open("confusion_matrix.csv", 'w') as csvfile:
-		csvwriter = csv.writer(csvfile)
-		csvwriter.writerows(con_mat)
-	
+jarbas.save("trained_jarbas")
